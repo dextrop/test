b5581b002aa5e83b29433852ebf16cee6ba82945
Saurabh Pandey <saurabh@trillbit.com>
2021-09-25 15:25:03 +0530

    Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Uploadtos3Component } from './uploadtos3/uploadtos3.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Uploadtos3Component,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
