e669df338887df9adc345e6bc2590f30337a519b
Saurabh Pandey <saurabh@trillbit.com>
2021-09-25 15:29:42 +0530

    Changes

---
+++ src/app/uploadtos3/uploadtos3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-uploadtos3',
+  templateUrl: './uploadtos3.component.html',
+  styleUrls: ['./uploadtos3.component.css']
+})
+export class Uploadtos3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
